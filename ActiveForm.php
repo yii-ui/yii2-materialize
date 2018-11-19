4656ab6f06af931e19e6efb46b042fd158cad34b
Christoph Möke <christophmoeke@gmail.com>
2018-11-19 14:48:14 +0100

    Added support to prevent form submit with enter

--- ActiveForm.php
+++ ActiveForm.php
@@ -7,4 +7,27 @@ class ActiveForm extends \yii\widgets\ActiveForm
 
     public $errorCssClass = 'invalid';
     public $successCssClass = 'valid';
+    public $disableSubmitOnEnter = false;
+
+    public function run()
+    {
+        if (!empty($this->_fields)) {
+            throw new InvalidCallException('Each beginField() should have a matching endField() call.');
+        }
+
+        $content = ob_get_clean();
+        echo Html::beginForm($this->action, $this->method, $this->options);
+
+        if ($this->disableSubmitOnEnter) {
+            echo Html::submitInput('', ['style' => 'display:none', 'disabled' => true, 'aria-hidden' => true]);
+        }
+
+        echo $content;
+
+        if ($this->enableClientScript) {
+            $this->registerClientScript();
+        }
+
+        echo Html::endForm();
+    }
 }
